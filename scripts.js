9933053678526845df91a4f7603ded62aaffd4d8
JoaoLuizMelo <joaoluiz.civil@gmail.com>
2023-03-26 01:29:44 -0300

    Ajustando de forma dinamica a aplicação

--- scripts.js
+++ scripts.js
@@ -3,17 +3,18 @@ const select = document.getElementById("currencySelect")
 const baseSelect = document.getElementById("baseSelect")
 
 
-const convertValue = () => {
+const convertValue = async() => {
     const input = document.getElementsByTagName("input")[0].value
     const numberOne = document.getElementById("value1")
     const numberTwo = document.getElementById("value2")
     if (baseSelect.value === "R$ Real Brasileiro") {
 
-        const dollarValue = 5.2
-        const euroValue = 5.9
+        const data = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL').then(response =>response.json())
+        const dollarValue = data.USDBRL.high
+        const euroValue = data.EURBRL.high
         const realBrasileiro = 1
-        const bitcon = 116625.61
-
+        const bitcon = data.BTCBRL.high
+        
 
         numberOne.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(input)
 
@@ -35,12 +36,12 @@ const convertValue = () => {
         }
     }
     if (baseSelect.value === "US$ Dolar Americano") {
-
-        const dollarValue = (5.2/5.2)
-        const euroValue = (5.9/5.2)
-        const realBrasileiro = (1/5.2)
-        const bitcon = (116625.61/5.2)
-
+        const data = await fetch('https://economia.awesomeapi.com.br/last/BRL-USD,EUR-USD,BTC-USD').then(response =>response.json())
+        const dollarValue = 1
+        const euroValue = data.EURUSD.high
+        const realBrasileiro = data.BRLUSD.high
+        const bitcon = data.BTCUSD.high
+        
 
         numberOne.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(input)
 
@@ -62,13 +63,12 @@ const convertValue = () => {
         }
     }
     if (baseSelect.value === "€ Euro") {
-
-        const dollarValue = (5.2/5.9)
-        const euroValue = (5.9/5.9)
-        const realBrasileiro = (1/5.9)
-        const bitcon = (116625.61/5.9)
-
-
+        const data = await fetch('https://economia.awesomeapi.com.br/last/BRL-EUR,USD-EUR,BTC-EUR').then(response =>response.json())
+        const dollarValue = data.USDEUR.high
+        const euroValue = 1
+        const realBrasileiro = data.BRLEUR.high
+        const bitcon = data.BTCEUR.high
+       
         numberOne.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(input)
 
         if (select.value === "US$ Dolar Americano") {
@@ -89,12 +89,12 @@ const convertValue = () => {
         }
     }
     if (baseSelect.value === "Bitcoin") {
-
-        const dollarValue = (5.2/116625.61)
-        const euroValue = (5.9/116625.61)
-        const realBrasileiro = (1/116625.61)
-        const bitcon = (116625.61/116625.61)
-
+        const data = await fetch('https://economia.awesomeapi.com.br/last/BTC-BRL,BTC-USD,BTC-EUR').then(response =>response.json())
+        const dollarValue = (1/parseFloat(data.BTCUSD.high))
+        const euroValue = (1/parseFloat(data.BTCEUR.high))
+        const realBrasileiro = (1/parseFloat(data.BTCBRL.high))
+        const bitcon = 1
+        
 
         numberOne.innerHTML = new Intl.NumberFormat({ style: 'decimal'}).format(input)
 
